9869c48645a07fd66c2929148cdd7da3c545aa42
Nicolás Fajardo Ramírez <n.fajardor@uniandes.edu.co>
2021-03-16 20:52:56 -0500

    arnol nos ignora :'v
    3 hand shake hecho

--- src/cliente/Client.java
+++ src/cliente/Client.java
@@ -2,6 +2,7 @@ package cliente;
 
 import java.io.*;
 import java.net.*;
+import java.util.Random;
 import java.util.Scanner;
 public class Client {
 	public static void main(String[] args) throws Exception{
@@ -11,6 +12,43 @@ public class Client {
 		OutputStream out = s.getOutputStream();
 		Scanner scan = new Scanner(System.in);
 		byte[] b = new byte[2002];
+		Random rand = new Random();
+		String id;
+		//Three hand shake
+		
+		/* Envio del SYN */
+		int synint = rand.nextInt(9000) + 1000;
+		String syn = synint + "";
+		System.out.println("Se va a mandar el SYN: " + syn);
+		out.write(syn.getBytes());
+		System.out.println("Se mandó el SYN: " + syn);
+		
+		/* recepcion del SYN y ACK */
+		in.read(b,0,b.length);
+		String synack = new String(b);
+		synack = synack.trim();
+		System.out.println("Se recibió el synack: " + synack);
+		int ack = Integer.parseInt(synack.split("-")[0]);
+		int syn2 = Integer.parseInt(synack.split("-")[1]);
+		if(!(syn2==synint+1)) {
+			String e = -1+"";
+			out.write(e.getBytes());
+			System.err.println("Connection error");
+			fin = true;
+		}
+		ack++;
+		
+		/* Envío del ultimo ACK */
+		if(!fin) {
+			String sack = ack+"";
+			out.write(sack.getBytes());
+		}
+		
+		
+		in.read(b, 0, b.length);
+		id = (new String(b)).trim();
+		out.write(id.getBytes());
+		
 		while(!fin) {
 			in.read(b, 0, b.length);
 			String str = new String(b);
--- src/servidor/ServerThread.java
+++ src/servidor/ServerThread.java
@@ -1,6 +1,7 @@
 package servidor;
 
 import java.net.*;
+import java.util.Random;
 import java.io.*;
 
 public class ServerThread extends Thread{
@@ -29,6 +30,41 @@ public class ServerThread extends Thread{
 			byte[] b = new byte[270000];
 			System.out.println("Thread " + id+" va a mandar un mensaje");
 			String s = "Bienvenido, soy el thread " + id + ". Por favor seleccione la opcion:\nA-Enviar Archivo de 100MB\nB-Enviar Archivo de 250MB\nC-Terminar la conexion ";
+			
+			//Three hand shaking
+			
+			/* Recepción del SYN */
+			System.out.println("Se va a recibir el SYN");
+			in.read(b, 0, b.length);
+			String syn = new String(b);
+			syn = syn.trim();
+			int synint = Integer.parseInt(syn);
+			
+			/* Envío del SYN con el ACK */
+			synint++;
+			Random rand = new Random();
+			int ack = rand.nextInt(9000) + 1000;
+			String synack = ack+"-"+synint;
+			System.out.println("Se va a enviar la confirmacion: "+synack);
+			out.write(synack.getBytes());
+			
+			/* Recepción del último ACK */
+			in.read(b,0,b.length);
+			String sack = new String(b);
+			sack = sack.trim();
+			System.out.println("Se recibio: "+sack);
+			int ack2 = Integer.parseInt(sack);
+			if(!(ack2 == ack+1)) {
+				String e = "Connection error";
+				out.write(e.getBytes());
+				throw new Exception(e);
+			};
+			
+			String giveId = id+"";
+			out.write(giveId.getBytes());
+			in.read(b,0,b.length);
+			
+			
 			while(!fin) {
 				out.write(s.getBytes());
 				in.read(b, 0 , b.length);
@@ -50,7 +86,7 @@ public class ServerThread extends Thread{
 			in.close();
 			out.close();
 		}catch(Exception e) {
-			System.err.println("Ha ocurrido un error.");
+			System.err.println("Ha ocurrido un error: " + e.getMessage());
 		}
 		servidor.eliminarThreadPorId(id);
 	}
